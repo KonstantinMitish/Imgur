54902ba1b17c571bf97dcec4418fa561d29d65a2
KonstantinMitish <mitikostya14@gmail.com>
2019-06-01 13:51:09 +0200

    Upload files

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Imgur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.components = new System.ComponentModel.Container();
+      this.button = new System.Windows.Forms.Button();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.SuspendLayout();
+      // 
+      // button
+      // 
+      this.button.Dock = System.Windows.Forms.DockStyle.Top;
+      this.button.Location = new System.Drawing.Point(0, 0);
+      this.button.Name = "button";
+      this.button.Size = new System.Drawing.Size(284, 48);
+      this.button.TabIndex = 1;
+      this.button.Text = "START";
+      this.button.UseVisualStyleBackColor = true;
+      this.button.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 1;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Controls.Add(this.button);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.Click += new System.EventHandler(this.Form1_Click);
+      this.ResumeLayout(false);
+
+        }
+
+    #endregion
+
+    private System.Windows.Forms.Button button;
+    private System.Windows.Forms.Timer timer1;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Net;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Imgur
+{
+  public partial class Form1 : Form
+  {
+     string[] chars= {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+                      "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                      "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+     public Form1()
+     {
+         InitializeComponent();
+     }
+
+     private void Form1_Load(object sender, EventArgs e)
+     {
+
+     }
+
+    Random rnd = new Random();
+    private void Form1_Click(object sender, EventArgs e)
+    {
+
+
+    }
+
+    private static bool on = false;
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if (on)
+      {
+        timer1.Stop();
+        button.Text = "START";
+        on = false;
+      }
+      else
+      {
+        timer1.Start();
+        button.Text = "STOP";
+        on = true;
+      }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      if (!Directory.Exists("img"))
+        Directory.CreateDirectory("img");
+      string file = chars[rnd.Next(chars.Length)] + chars[rnd.Next(chars.Length)] +
+                  chars[rnd.Next(chars.Length)] + chars[rnd.Next(chars.Length)] + chars[rnd.Next(chars.Length)] +
+                  ".jpg";
+      Uri url = new Uri("https://i.imgur.com/" + file);
+      //url = new Uri("https://i.imgur.com/11111.jpg");
+      var request = (HttpWebRequest)WebRequest.Create(url);
+      request.Method = "HEAD";
+      request.AllowAutoRedirect = false;
+      
+      using (var response = request.GetResponse() as HttpWebResponse)
+      {
+        if (response.StatusCode != HttpStatusCode.Redirect)
+        {
+          WebClient client = new WebClient();
+          client.DownloadFile(url, "img/" + file);
+          this.BackgroundImage = Image.FromFile("img/" + file);
+        }
+      }
+    }
+  }
+}
